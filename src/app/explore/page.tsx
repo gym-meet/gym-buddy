795e75c82fb9ce787f99289d7cbafa7526a58bfd
kathrynan <kanakagawa2021@gmail.com>
2025-05-07 18:08:26 -1000

    Added link to profile cards
    NOT COMPLETE DO NOT MERGE INTO MAIN

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { FunnelFill } from 'react-bootstrap-icons';
+import Link from 'next/link';
 import { prisma } from '@/lib/prisma';
 import { ProfileCardData } from '@/lib/ProfileCardData';
 import TopMenu from '../../components/TopMenu';
@@ -53,7 +54,14 @@ const Explore = async () => {
         <Row xs={1} sm={2} lg={3} xl={4} className="g-4">
           {users.length > 0 ? (
             users.map((profile) => (
-              <ProfileCard key={profile.id} profile={profile} />
+              // Link dynamic ID for each profile card
+              <Link
+                href={`/profile/${users.id}`}
+                key={users.id}
+                style={{ textDecoration: 'none', color: 'inherit' }}
+              >
+                <ProfileCard key={profile.id} profile={profile} />
+              </Link>
             ))
           ) : (
             <Col>
